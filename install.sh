73cc62e7e911af255539a7a1196016a0467bfc26
jigmi <133286016+jigmi0@users.noreply.github.com>
2026-04-11 11:37:32 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+"""
+install.sh - installs en-linux so anyone can use it with one command
+
+What this script does:
+1. Checks Python and git are installed
+2. Downloads code from GitHub
+3. Installs the Python dependencies into an folder
+4. Creates the `en-linux` terminal command
+
+"""
